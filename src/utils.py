ff45351b3180acdb996e38dd4eaf42ba66b578aa
Owais Mujtaba <owais.mujtaba123@gmail.com>
2024-05-24 23:29:31 +0200

    moved map_eeg function to the utils

--- src/utils.py
+++ src/utils.py
@@ -23,44 +23,6 @@ def find_closest_starting_point_in_eeg(eeg_events, timestamp):
     return close_index
 
 
-def map_eeg_actions_to_marker_words(start_timestamp_eeg, eeg_events, markers_words_timestamps):
-    """
-        Maps EEG actions to corresponding marker words and timestamps.
-
-        Args:
-            - start_timestamp_eeg (int): The starting index for processing EEG events.
-            - eeg_events (list of tuples): A list of EEG events where each event is represented as a tuple:
-                (action, start_time, end_time, start_index, end_index, duration).
-            - markers_words_timestamps (list of tuples): A list of markers with corresponding words and timestamps,
-                where each item is represented as a tuple: (marker, word, time).
-
-            Returns:
-            - result (list of lists): A list of mapped actions to markers with their corresponding details,
-                where each item is represented as a list:
-                [marker, word, start_time, end_time, start_index, end_index, duration, time].
-
-    """
-
-    word_index = 0
-    result = []
-
-    for event in eeg_events[start_timestamp_eeg:]:
-        action, start_time, end_time, start_index, end_index, duration = event
-
-        for index in range(word_index, len(markers_words_timestamps)):
-            marker, word, time = markers_words_timestamps[index]
-
-            if action == marker:
-                result.append([marker, word, start_time, end_time, start_index, end_index, duration, time])
-                word_index = index + 1
-                break
-    
-
-    columns = ['marker', 'word', 'start_time_eeg', 'end_time_eeg', 'start_index_eeg', 'end_index_eeg', 'duration_eeg', 'time_xdf']
-    data = pd.DataFrame(result, columns=columns)
-    data.to_csv('mappings.csv')
-    return result
-
 
 
 
