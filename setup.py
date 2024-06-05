e741d3be030f2302d66a0700895800170b23461e
Owais Mujtaba <owais.mujtaba123@gmail.com>
2024-06-05 22:26:18 +0200

    added license

--- setup.py
+++ setup.py
@@ -1,39 +1,34 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = '-e .'
-
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
-        list of requirements
+    list of requirements
     '''
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace('\n', '') for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
+    return requirements
 
 setup(
     name='EEG_AUDIO_Anotator',
     version='1.0.0',
-    author = 'Owais Mujtaba Khanday',
-    author_email = 'owais.mujtaba123@gmail.com',
+    author='Owais Mujtaba Khanday',
+    author_email='owais.mujtaba123@gmail.com',
     url='https://github.com/owaismujtaba/EEGAnotator',
-    package_dir={'': 'src'},  
-    packages=find_packages(where='src'), 
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GPL-3.0 License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.10.14',
-    install_requires=get_requirements(),  
+    install_requires=get_requirements('requirements.txt'),
     entry_points={
         'console_scripts': [
-            'eeganotate=main:main',
+            'eeganotate=src.main:main',
         ],
     },
     include_package_data=True,
@@ -42,4 +37,5 @@ setup(
         'classes': ['*.msg'],
         'gui': ['*.msg'],
     },
+    license='GPL-3.0'
 )
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QApplication
-from gui.main_display import MainWindow
-from classes.eeg import EegData
 import sys
 
-if __name__ == '__main__':
+def main():
     App = QApplication(sys.argv)
     MainWindow = MainWindow()
     MainWindow.show()
     sys.exit(App.exec_())
+
+
+if __name__ == '__main__':
+    main()
