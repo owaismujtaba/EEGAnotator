6560d35876b391ce52e2f5bda2c5575c76f6b9d6
Owais Mujtaba <owais.mujtaba123@gmail.com>
2024-06-05 20:22:59 +0300

    save file done bids format

--- src/main.py
+++ src/main.py
@@ -1,16 +1,9 @@
 from PyQt5.QtWidgets import QApplication
-#from src.gui.main_display1 import MainWindow
 from gui.main_display import MainWindow
 from classes.eeg import EegData
-import pdb
 import sys
 
 if __name__ == '__main__':
-
-    edfFilePath = r'C:\DeepRESTORE\\Last\Data\\F10\\PictureNaming\\eeg\\f10.edf'
-
-    #data = EegData(edfFilePath)
-    #pdb.set_trace()
     App = QApplication(sys.argv)
     MainWindow = MainWindow()
     MainWindow.show()
