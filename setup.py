71b1bd1c79fc7c69cbea77e4aefb3121ec411f5c
Owais Mujtaba <owais.mujtaba123@gmail.com>
2024-06-05 20:10:12 +0200

    fixing start module

--- setup.py
+++ setup.py
@@ -15,16 +15,31 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
+
 setup(
     name='EEG_AUDIO_Anotator',
     version='1.0.0',
     author = 'Owais Mujtaba Khanday',
     author_email = 'owais.mujtaba123@gmail.com',
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt'),
+    url='https://github.com/owaismujtaba/EEGAnotator',
+    package_dir={'': 'src'},  
+    packages=find_packages(where='src'), 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10.14',
+    install_requires=get_requirements(),  
     entry_points={
-        "console_scripts": [
-            "eeganotate=src.main:main",
-        ]
-    }
+        'console_scripts': [
+            'eeganotate=main:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['*.txt', '*.rst'],
+        'classes': ['*.msg'],
+        'gui': ['*.msg'],
+    },
 )
